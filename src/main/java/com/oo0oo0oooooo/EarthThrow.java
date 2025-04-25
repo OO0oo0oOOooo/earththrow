c38acd6d2b8cc1bc0f70d49559ffe5c7704e996f
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2025-04-24 19:02:44 -0600

    Initial Commit

---
+++ src/main/java/com/oo0oo0oooooo/EarthThrow.java
@@ -0,0 +1,205 @@
+package com.oo0oo0oooooo;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
+
+import com.projectkorra.projectkorra.GeneralMethods;
+import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.AddonAbility;
+import com.projectkorra.projectkorra.ability.EarthAbility;
+import com.projectkorra.projectkorra.configuration.ConfigManager;
+import com.projectkorra.projectkorra.earthbending.passive.DensityShift;
+import com.projectkorra.projectkorra.region.RegionProtection;
+import com.projectkorra.projectkorra.util.BlockSource;
+import com.projectkorra.projectkorra.util.ClickType;
+import com.projectkorra.projectkorra.util.TempBlock;
+
+public class EarthThrow extends EarthAbility implements AddonAbility {
+
+    private long COOLDOWN;
+    private double HIT_RADIUS;
+    private double SOURCE_RANGE;
+    private double LAUNCH_STRENGTH;
+    
+    private Location _location = null;
+    private Block _sourceBlock = null;
+    private Material _sourceType = null;
+    private boolean _launchCodesConfirmed = false;
+
+    private Listener _earthThrowListener;
+
+    public EarthThrow(Player player) {
+        super(player);
+
+        if (!bPlayer.canBend(this)) return;
+		if (bPlayer.isOnCooldown(this)) return;
+
+        EarthThrow existing = getAbility(player, getClass());
+        if(existing != null) {
+            existing.remove();
+            return;
+        }
+
+        setField();
+
+        if (prepare()) {
+            focusBlock();
+            start();
+		}
+    }
+
+    @Override
+    public void progress() {
+        if (player.isDead() || !player.isOnline()) {
+			remove();
+			return;
+		}
+
+        if (RegionProtection.isRegionProtected(this.player, _location)) {
+            remove();
+            return;
+        }
+
+        if(_launchCodesConfirmed) {
+            launch();
+            remove();
+            return;
+        }
+    }
+
+    public void setField() {
+        COOLDOWN = ConfigManager.getConfig().getLong("ExtraAbilities.Jahko.Earth.EarthThrow.Cooldown");
+		SOURCE_RANGE = ConfigManager.getConfig().getDouble("ExtraAbilities.Jahko.Earth.EarthThrow.SourceRange");
+		LAUNCH_STRENGTH = ConfigManager.getConfig().getDouble("ExtraAbilities.Jahko.Earth.EarthThrow.LaunchStrength");
+		HIT_RADIUS = ConfigManager.getConfig().getDouble("ExtraAbilities.Jahko.Earth.EarthThrow.HitRadius");
+    }
+
+    public void onClick() {
+        _launchCodesConfirmed = true;
+    }
+    
+    private boolean prepare() {
+        final Block block = BlockSource.getEarthSourceBlock(player, SOURCE_RANGE, ClickType.SHIFT_DOWN);
+		if (block == null || !this.isEarthbendable(block)) {
+			return false;
+		} else if (TempBlock.isTempBlock(block) && !EarthAbility.isBendableEarthTempBlock(block)) {
+			return false;
+		}
+
+        if (RegionProtection.isRegionProtected(this.player, _location)) {
+            return false;
+        }
+
+        _sourceBlock = block;
+        return true;
+    }
+
+    private void focusBlock() {
+		if (DensityShift.isPassiveSand(_sourceBlock)) {
+			DensityShift.revertSand(_sourceBlock);
+		}
+
+        switch (_sourceBlock.getType()) {
+            case SAND:
+			    _sourceType = Material.SAND;
+		    	_sourceBlock.setType(Material.SANDSTONE);
+                break;
+            case RED_SAND:
+		     	_sourceType = Material.RED_SAND;
+		        _sourceBlock.setType(Material.RED_SANDSTONE);
+                break;
+            case STONE:
+            	_sourceBlock.setType(Material.COBBLESTONE);
+            	_sourceType = Material.STONE;
+                break;
+            default:
+    			_sourceType = _sourceBlock.getType();
+    			_sourceBlock.setType(Material.STONE);
+                break;
+        }
+
+		_location = _sourceBlock.getLocation();
+	}
+
+    private void unfocusBlock() {
+        if (this._sourceBlock != null) {
+    		this._sourceBlock.setType(this._sourceType);
+            _sourceBlock = null;
+        }
+	}
+
+    private void launch() {
+        Vector dir = this.player.getEyeLocation().getDirection().clone().normalize().multiply(LAUNCH_STRENGTH);
+        for (final Entity entity : GeneralMethods.getEntitiesAroundPoint(this._location, HIT_RADIUS)) {
+            GeneralMethods.setVelocity(this, entity, dir);
+        }
+
+        playEarthbendingSound(_location);
+        bPlayer.addCooldown(this);
+    }
+
+    @Override
+    public void remove() {
+        super.remove();
+        unfocusBlock();
+    }
+
+    @Override
+    public boolean isSneakAbility() {
+        return true;
+    }
+
+    @Override
+    public boolean isHarmlessAbility() {
+        return true;
+    }
+
+    @Override
+    public long getCooldown() {
+        return COOLDOWN;
+    }
+
+    @Override
+    public String getName() {
+        return "EarthThrow";
+    }
+
+    @Override
+    public Location getLocation() {
+        return player.getLocation();
+    }
+
+    @Override
+    public void load() {
+        _earthThrowListener = new EarthThrowListener();
+        ProjectKorra.plugin.getServer().getPluginManager().registerEvents(_earthThrowListener, ProjectKorra.plugin);
+
+		ConfigManager.getConfig().addDefault("ExtraAbilities.Jahko.Earth.EarthThrow.Cooldown", 1000);
+		ConfigManager.getConfig().addDefault("ExtraAbilities.Jahko.Earth.EarthThrow.SourceRange", 10);
+		ConfigManager.getConfig().addDefault("ExtraAbilities.Jahko.Earth.EarthThrow.LaunchStrength", 2);
+		ConfigManager.getConfig().addDefault("ExtraAbilities.Jahko.Earth.EarthThrow.HitRadius", 2);
+		ConfigManager.defaultConfig.save();
+    }
+
+    @Override
+    public void stop() {
+        HandlerList.unregisterAll(_earthThrowListener);
+        super.remove();
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Jahko";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
--- src/main/java/com/oo0oo0oooooo/EarthThrowListener.java
+++ src/main/java/com/oo0oo0oooooo/EarthThrowListener.java
@@ -10,21 +10,26 @@ import org.bukkit.event.player.PlayerToggleSneakEvent;
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ability.CoreAbility;
 
-public class DirtListener implements Listener {
+public class EarthThrowListener implements Listener {
 
     @EventHandler
     public void onSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
         BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
 
+        if(player == null) return;
         if(bPlayer == null) return;
         if(event.isCancelled()) return;
         if(player.isSneaking()) return;
 
-        if (bPlayer.getBoundAbilityName().equalsIgnoreCase("Dirt")) {
-            if(CoreAbility.getAbility(player, Dirt.class) == null) {
-                new Dirt(player);
+        if (bPlayer.getBoundAbilityName().equalsIgnoreCase("EarthThrow")) {
+            EarthThrow earthThrow = CoreAbility.getAbility(player, EarthThrow.class);
+            if(earthThrow != null) {
+                earthThrow.remove();
+                earthThrow = null;
             }
+            
+            new EarthThrow(player);
         }
     }
 
@@ -38,13 +43,13 @@ public class DirtListener implements Listener {
         Player player = event.getPlayer();
         BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
         
+        if(player == null) return;
         if(bPlayer == null) return;
 
-        Dirt dirt = CoreAbility.getAbility(player, Dirt.class);
-        if (dirt != null) {
-            dirt.onClick();
+        EarthThrow earthThrow = CoreAbility.getAbility(player, EarthThrow.class);
+        if (earthThrow != null) {
+            earthThrow.onClick();
         }
-    }    
-         
+    }
 }        
          
